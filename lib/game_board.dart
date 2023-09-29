c207163b4f409e8120f95a5c9d7594b4a7bda9a5
Gleb Shalimov <sbgleb10@gmail.com>
2023-09-29 10:06:48 +0300

    placed pieces on game goard

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -8,26 +8,129 @@ class GameBoard extends StatefulWidget {
   const GameBoard({super.key});
 
   @override
-  State<GameBoard> createState() => _GameBoardState(
-
-  // Initialize Board
-  void _initializeBoard() {
-  );
-
+  State<GameBoard> createState() => _GameBoardState();
 }
 
 class _GameBoardState extends State<GameBoard> {
   late List<List<ChessPiece?>> board;
 
-
+  @override
+  void initState() {
+    super.initState();
+    _initializeBoard();
   }
 
-  // Create a chess piece
-  ChessPiece myPawn = ChessPiece(
-    type: ChessPieceType.pawn,
-    isWhite: false,
-    imagePath: 'lib/images/pawn.png',
-  );
+  // Initialize Board
+  void _initializeBoard() {
+    // initialize the board with nulls, meaning no pieces in those position
+
+    List<List<ChessPiece?>> newBoard =
+        List.generate(8, (index) => List.generate(8, (index) => null));
+
+    // Place pawn
+    for (int i = 0; i < 8; i++) {
+      newBoard[1][i] = ChessPiece(
+        type: ChessPieceType.pawn,
+        isWhite: false,
+        imagePath: 'lib/images/pawn.png',
+      );
+      newBoard[6][i] = ChessPiece(
+        type: ChessPieceType.pawn,
+        isWhite: true,
+        imagePath: 'lib/images/pawn.png',
+      );
+    }
+
+    // Place rooks
+    newBoard[0][0] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: false,
+      imagePath: 'lib/images/rook.png',
+    );
+    newBoard[0][7] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: false,
+      imagePath: 'lib/images/rook.png',
+    );
+    newBoard[7][0] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: 'lib/images/rook.png',
+    );
+    newBoard[7][7] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: 'lib/images/rook.png',
+    );
+    // Place knights
+    newBoard[0][1] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: 'lib/images/knight.png',
+    );
+    newBoard[0][6] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: 'lib/images/knight.png',
+    );
+    newBoard[7][1] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: 'lib/images/knight.png',
+    );
+    newBoard[7][6] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: 'lib/images/knight.png',
+    );
+    // Place bishops
+    newBoard[0][2] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: 'lib/images/bishop.png',
+    );
+    newBoard[0][5] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: 'lib/images/bishop.png',
+    );
+    newBoard[7][2] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: 'lib/images/bishop.png',
+    );
+    newBoard[7][5] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: 'lib/images/bishop.png',
+    );
+
+    // Place queens
+    newBoard[0][3] = ChessPiece(
+      type: ChessPieceType.queen,
+      isWhite: false,
+      imagePath: 'lib/images/queen.png',
+    );
+    newBoard[7][3] = ChessPiece(
+      type: ChessPieceType.queen,
+      isWhite: true,
+      imagePath: 'lib/images/queen.png',
+    );
+
+    // Place kings
+    newBoard[0][4] = ChessPiece(
+      type: ChessPieceType.king,
+      isWhite: false,
+      imagePath: 'lib/images/king.png',
+    );
+    newBoard[7][4] = ChessPiece(
+      type: ChessPieceType.king,
+      isWhite: true,
+      imagePath: 'lib/images/king.png',
+    );
+
+    board = newBoard;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,9 +142,13 @@ class _GameBoardState extends State<GameBoard> {
         gridDelegate:
             const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
         itemBuilder: (context, index) {
+          // get the row and the col position of this square
+
+          int row = index ~/ 8;
+          int col = index % 8;
           return Square(
             isWhite: isWhite(index),
-            piece: myPawn,
+            piece: board[row][col],
           );
         },
       ),
